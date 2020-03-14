9df3384af5f681434052a711158df9cfca4087e7
Corey Beres <corey.beres@gmail.com>
2020-03-14 02:35:14 -0400

    holiday picker improvements

--- app/src/main/java/com/spinthechoice/garbage/android/HolidayPickerAdapter.java
+++ app/src/main/java/com/spinthechoice/garbage/android/HolidayPickerAdapter.java
@@ -10,16 +10,10 @@ import android.widget.TextView;
 
 import java.util.List;
 
-class HolidayPickerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+class HolidayPickerAdapter extends RecyclerView.Adapter<HolidayPickerAdapter.ItemViewHolder> {
     @FunctionalInterface
     interface OnChangeListener {
         void changed(String id, boolean postpone, boolean cancel);
-    }
-
-    static class HeaderViewHolder extends RecyclerView.ViewHolder {
-        HeaderViewHolder(final RelativeLayout layout) {
-            super(layout);
-        }
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
@@ -38,11 +32,6 @@ class HolidayPickerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }
     }
 
-    private static final int HEADER_INDEX = 0;
-    private static final int HEADER_COUNT = 1;
-    private static final int HEADER_TYPE = 0;
-    private static final int ITEM_TYPE = 1;
-
     private final List<HolidayPickerItem> holidays;
     private OnChangeListener listener;
 
@@ -55,35 +44,19 @@ class HolidayPickerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     @Override
-    public int getItemViewType(final int position) {
-        return position == HEADER_INDEX ? HEADER_TYPE : ITEM_TYPE;
+    public ItemViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.holiday_picker_item, parent, false);
+        TextView holiday = v.findViewById(R.id.text_holiday);
+        TextView date = v.findViewById(R.id.text_date);
+        CheckBox postpone = v.findViewById(R.id.check_postpone);
+        CheckBox cancel = v.findViewById(R.id.check_cancel);
+        return new ItemViewHolder(v, holiday, date, postpone, cancel);
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        if (viewType == HEADER_TYPE) {
-            RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.holiday_picker_header, parent, false);
-            return new HeaderViewHolder(v);
-        } else {
-            RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.holiday_picker_item, parent, false);
-            TextView holiday = v.findViewById(R.id.text_holiday);
-            TextView date = v.findViewById(R.id.text_date);
-            CheckBox postpone = v.findViewById(R.id.check_postpone);
-            CheckBox cancel = v.findViewById(R.id.check_cancel);
-            return new ItemViewHolder(v, holiday, date, postpone, cancel);
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder baseHolder, final int position) {
-        if (position == HEADER_INDEX) {
-            return; // skip the header
-        }
-
-        final HolidayPickerItem item = holidays.get(position - HEADER_COUNT);
-        final ItemViewHolder holder = (ItemViewHolder) baseHolder;
+    public void onBindViewHolder(final ItemViewHolder holder, final int position) {
+        final HolidayPickerItem item = holidays.get(position);
         holder.holiday.setText(item.getName());
         holder.date.setText(item.getDateText());
         holder.postpone.setChecked(item.isPostpone());
@@ -126,6 +99,6 @@ class HolidayPickerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public int getItemCount() {
-        return holidays.size() + HEADER_COUNT;
+        return holidays.size();
     }
 }
